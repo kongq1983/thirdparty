e58702d8ed269889771d878365164bcc1d77db5e
kq <kongq1983@yeah.net>
2019-04-03 00:01:19 +0800

    add ehcache demo

---
+++ ehcache/src/main/java/com/kq/ehcache/demo/Demo.java
@@ -0,0 +1,40 @@
+package com.kq.ehcache.demo;
+
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+
+
+/**
+ * Created by qikong on 2019/4/2.
+ */
+public class Demo {
+
+
+    public static void main(String[] args) {
+
+        CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+                .withCache("preConfigured",
+                        CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
+                                ResourcePoolsBuilder.heap(100))
+                                .build())
+                .build(true);
+
+        Cache<Long, String> preConfigured
+                = cacheManager.getCache("preConfigured", Long.class, String.class);
+
+        Cache<Long, String> myCache = cacheManager.createCache("myCache",
+                CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
+                        ResourcePoolsBuilder.heap(100)).build());
+
+        myCache.put(1L, "da one!");
+        String value = myCache.get(1L);
+
+        System.out.println("value="+value);
+
+        cacheManager.close();
+    }
+
+}
