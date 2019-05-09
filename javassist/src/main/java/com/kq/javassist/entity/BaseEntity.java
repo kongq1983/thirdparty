faf2a7013ff2fba113317c0905e144176ef965aa
king <king@qq.com>
2019-05-09 19:05:21 +0800

    add javassist demo

---
+++ javassist/src/main/java/com/kq/javassist/entity/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.kq.javassist.entity;
+
+/**
+ * BaseEntity
+ *
+ * @author kq
+ * @date 2019-05-09
+ */
+public class BaseEntity {
+
+    private String createUser;
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+}
---
+++ javassist/src/main/java/com/kq/javassist/entity/Company.java
@@ -0,0 +1,39 @@
+package com.kq.javassist.entity;
+
+/**
+ * Company
+ *
+ * @author kq
+ * @date 2019-05-09
+ */
+public class Company extends BaseEntity {
+
+    private Long id;
+    private String name;
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
